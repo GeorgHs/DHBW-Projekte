107edcb22b91dc5b008570467bc278e357fa4482
Tim-Obert <tim-obert@web.de>
2019-05-16 09:05:57 +0200

    Merge branch 'master' of C:\Users\tim-o\Projekte\DHBW-Projekte with conflicts.

--- src/controller/ApiServlet.java
+++ src/controller/ApiServlet.java
@@ -1,11 +1,10 @@
-package controller;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.reflect.Method;
 
 @WebServlet(name = "controller.ApiServlet", urlPatterns={"/api/*"})
