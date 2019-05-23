12041d732bded4b3cc541cb8a14fda5c6fd7f83b
burgha <21040544+burgha@users.noreply.github.com>
2019-05-23 16:08:07 +0200

    extended websocket functionality

--- src/controller/WebsocketController.java
+++ src/controller/WebsocketController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import com.auth0.jwt.interfaces.DecodedJWT;
-
 import javax.websocket.*;
 import javax.websocket.server.*;
 import java.io.IOException;
@@ -21,11 +20,11 @@ public class WebsocketController {
 
     @OnMessage
     public void onMessage(String message, final Session session) {
-        System.out.println("Message from " + session.getId() + ": " + message);
+        System.out.println("Message from " + session.getId() + "/" + SessionController.getIdByWebsocketSession(session) + ": " + message);
     }
 
     @OnClose
-    public void onClose(Session session) throws IOException {
+    public void onClose(Session session) {
         // WebSocket connection closes
         String token = session.getRequestParameterMap().get("token").get(0);
         DecodedJWT jwt = SessionController.decodeJWT(token);
@@ -37,5 +36,26 @@ public class WebsocketController {
     @OnError
     public void onError(Session session, Throwable throwable) {
         // Do error handling here
+    }
+
+    public static void sendMessage(String user_identifier, String message) {
+        Session session = SessionController.getWebsocketSessionById(user_identifier);
+        if (session != null) {
+            try {
+                session.getBasicRemote().sendText(message);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void broadcast(String message) {
+        for (Session session : SessionController.getWebsocketSessions().values()) {
+            try {
+                session.getBasicRemote().sendText(message);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
