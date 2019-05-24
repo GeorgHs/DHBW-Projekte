d0d4f1433b8a5d189038a8c68ea380942ce1b424
Tim-Obert <tim-obert@web.de>
2019-05-24 08:34:38 +0200

    Datenbank connection

--- src/controller/DatabaseController.java
+++ src/controller/DatabaseController.java
@@ -1,22 +1,66 @@
 package controller;
 
+import org.apache.catalina.connector.Response;
+
+import javax.script.ScriptContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.mysql.jdbc.Driver;
 
 @WebListener
 public class DatabaseController implements ServletContextListener {
+
+    private static Connection con;
+    private Response response;
+
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         // This function runs on server startup. Connect to database here
+        try {
+            String dbDriver = "com.mysql.jdbc.Driver";
+            String dbURL = "jdbc:mysql://wsn.hburger.space:3306/";
+            // Database name to access
+            String dbName = "social_network?serverTimezone=UTC";
+            String dbUsername = "root";
+            String dbPassword = "webengineering";
 
+
+            Class driver = com.mysql.jdbc.Driver.class;
+            Class.forName(dbDriver);
+            con = DriverManager.getConnection(dbURL + dbName,
+                    dbUsername,
+                    dbPassword);
+        } catch (Exception e) {
+            System.out.println(e);
+            e.printStackTrace();
+
+        }
     }
 
     /**
      * Execute SQL Command
+     *
      * @param sql command to run
      */
-    public static void executeSQL(String sql) {
+    public static ResultSet executeSQL(String sql) {
+        try {
+            PreparedStatement st = con.prepareStatement(sql);
+            ResultSet resultSet = st.executeQuery();
+            return resultSet;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        return null;
     }
+
 }
--- src/models/Profile.java
+++ src/models/Profile.java
@@ -1,14 +1,32 @@
 package models;
 
+
+import controller.DatabaseController;
+
+import java.sql.ResultSet;
+
 public class Profile {
 
     private int user_id;
     private String username;
     private String email;
+    private DatabaseController db = new DatabaseController();
 
-    public int getUser_id() {
-        return 1;
-        //return user_id;
+    public Object getUser_id() {
+
+        try {
+        ResultSet resultSet = db.executeSQL("Select user_id from profiles where user_id=1;");
+
+            if(resultSet.next()) {
+
+                return resultSet.getInt("user_id");
+            }
+        }catch (Exception e){
+
+        }
+    return 0;
+             //return user_id;
+
     }
 
     public void setUser_id(int user_id) {
@@ -17,8 +35,17 @@ public class Profile {
 
     public String getUsername() {
 
-        return "Elon Musk";
-        //return username;
+        try {
+            ResultSet resultSet = db.executeSQL("Select username from profiles where user_id=1;");
+
+            if(resultSet.next()) {
+                System.out.println();
+                return resultSet.getString("username");
+            }
+        }catch (Exception e){
+
+        }
+        return "";
     }
 
     public void setUsername(String username) {
